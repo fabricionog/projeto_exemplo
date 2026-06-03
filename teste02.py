64cd7fb9244b1a9fa842eb32bdbb229eccfe9dc6
Fabricio Nogueira <freitas7114@gmail.com>
2026-06-03 14:06:44 -0300

    adicionei comentarios em teste02

--- teste02.py
+++ teste02.py
@@ -1,5 +1,8 @@
+# Recebe a idade do usuario como string
 age = input("Digite sua idade: ")
 
+# Faz "cast" da idade para inteiro e calcula
 age = int(age) + 10
 
+# exibe tudo "interpolando" 
 print(f"Daqui a 10 anos você terá {age} anos.")
